6f2d66e1dbbca91273f1415c3d057c87b00ca325
Kim Hoogenberg <kim.hoogenberg@nedap.com>
2014-08-01 21:14:53 +0200

    Added dutch locale support

---
+++ template/locales/nl.js
@@ -0,0 +1,19 @@
+define({
+	nl: {
+		"Compare all with predecessor": "Vergelijk alle met voorgaande versie", 
+		"compare changes to:"         : "vergelijk veranderingen met:",
+		"compared to"                 : "vergelijk met",
+		"Default value:"              : "Standaard waarde:",
+		"Description"                 : "Omschrijving",
+		"Field"                       : "Veld",
+		"General"                     : "Algemeen",
+		"Generated with"              : "Gegenereerd met",
+		"Name"                        : "Naam",
+		"No response values."         : "Geen response waardes.",
+		"optional"                    : "optioneel",
+		"Parameter"                   : "Parameter",
+		"Permission:"                 : "Permissie:",
+		"show up to version:"         : "toon tot en met versie:",
+		"Type"                        : "Type"
+	}
+});
